adbff999ceb252481ecc49cf6a9eeb54c3048ff3
yanditv <yankur44@gmail.com>
2025-09-30 09:59:11 -0500

    asas

--- junior.js
+++ junior.js
@@ -2,3 +2,8 @@ function Hello() {
     var mensaje = "Hello Junior Developer";
     console.log(mensaje);
 }
+
+function despedir() {
+    var mensaje = "Goodbye Junior Developer";
+    console.log(mensaje);
+}
